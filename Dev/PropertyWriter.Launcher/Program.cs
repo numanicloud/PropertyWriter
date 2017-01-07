b6131978c21cd92011d8479c83c7a505642ad570
NumAniCloud <num.ani.cloud@gmail.com>
2017-01-07 17:47:28 +0900

    Luncherを追加

---
+++ Dev/PropertyWriter.Launcher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyWriter.Launcher
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("PropertyWriter を起動中...");
+			Directory.SetCurrentDirectory("Core\\");
+			Process.Start("PropertyWriter.exe");
+		}
+	}
+}
